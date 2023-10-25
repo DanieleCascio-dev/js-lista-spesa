d3792e8e3a20297c5e3645c006fba25a4c1652b5
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-10-25 15:00:20 +0200

    Exercise complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+//Lista
+const list = document.querySelector("ul");
 
+//Array
+
+const toBuy = [
+  "biscotti",
+  "pasta",
+  "zucchine",
+  "peperoni",
+  "insalata",
+  "latte di soia",
+  "cereali",
+  "pane",
+  "cioccolata",
+];
+
+//Index
+let i = 0;
+
+//String
+let listToBuy = "";
+
+//While iteration
+
+while (i < toBuy.length) {
+  listToBuy += `<li>${toBuy[i]}</li>`;
+  i++;
+}
+
+//Print on page
+list.innerHTML = listToBuy;
